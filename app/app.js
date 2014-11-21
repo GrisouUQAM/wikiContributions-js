92eea52199f5b8541ea9530fc353edbe2533d4d6
Nilovna Bascunan-Vasquez <contact@nilovna.com>
2014-11-21 13:23:44 -0500

    Ajout contenu Advanced Search

--- app/app.js
+++ app/app.js
@@ -53,3 +53,36 @@ grisouApp.controller('AdvancedSearchCollapseCtrl', function ($scope) {
   $scope.isCollapsed = false;
 });
 
+grisouApp.controller('DatepickerDemoCtrl', function ($scope) {
+  $scope.today = function() {
+    $scope.dt = new Date();
+  };
+
+  $scope.clear = function () {
+    $scope.dt = null;
+  };
+
+  $scope.toggleMin = function() {
+    $scope.minDate = $scope.minDate ? null : new Date();
+  };
+  $scope.toggleMin();
+
+  $scope.open = function($event) {
+    $event.preventDefault();
+    $event.stopPropagation();
+
+    $scope.opened = true;
+  };
+
+  $scope.dateOptions = {
+    formatYear: 'yy',
+    startingDay: 1
+  };
+
+  $scope.formats = ['dd MMM yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+  $scope.format = $scope.formats[0];
+});
+
+grisouApp.controller('MinorEditCtrl', function($scope) {
+  $scope.minorEditModel = 'With';
+});
